7ffeefdfe7ff534bb3733701f459078b2305b241
WinifredKpochi <wimidoo3@gmail.com>
2022-03-11 07:24:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - entry point
  *
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
