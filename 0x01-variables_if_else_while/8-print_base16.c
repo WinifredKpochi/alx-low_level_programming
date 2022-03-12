e1ced47694fc1458e3df94149b887f70105e35d4
WinifredKpochi <wimidoo3@gmail.com>
2022-03-11 20:15:29 -0800

    print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,20 +10,19 @@ int main(void)
 	char ch = 'a';
 	int i = 0;
 
-	while(i< 16)
+	while (i < 16)
 	{
-		if(i< 10)
+		if (i < 10)
 		{
 			putchar(i + '0');
 		}
 		else
 		{
 			putchar(ch);
-			
 			ch++;
 		}
 		i++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
