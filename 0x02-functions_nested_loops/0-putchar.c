496bd820ece9fdc96a2d26bc665f669f114a3a17
WinifredKpochi <wimidoo3@gmail.com>
2022-03-15 11:26:51 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char c(8) = "_putchar";
 
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c(i));
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
